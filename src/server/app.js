f2b88494b85a08f41cbf78181dd608ccbcdce673
Kristoffer Alquiza <kalquiza@gmail.com>
2020-11-09 19:37:44 -0800

    Add express web framework
    Replaces node http module with express to create a server for our
    application.

--- src/server/app.js
+++ src/server/app.js
@@ -1,14 +1,15 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.use(express.static('dist'))
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+console.log(__dirname)
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.get('/', function (req, res) {
+  res.sendFile('dist/index.html')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
