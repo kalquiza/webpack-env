b6d1da4e5b04401ff745fa58ea118089ece1cadd
Kristoffer Alquiza <kalquiza@gmail.com>
2020-11-17 14:10:23 -0800

    Minify webpack output for production
    Include and configure TerserWebpackPlugin, MiniCssExtractPlugin,
    CssMinimizerWebpackPlugin for production optimization.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,21 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+//installed via npm
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
 const webpack = require('webpack'); //to access built-in plugins
 const path = require('path');
 
 module.exports = {
     mode: 'production',
     entry: './src/client/index.js',
+    optimization: {
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin(),
+        ],
+    },
     module: {
         rules: [
             {
@@ -18,15 +29,8 @@ module.exports = {
                 }
             },
             {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
+                test: /.s?css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             },
         ]
     },
@@ -34,7 +38,11 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
     ],
     output: {
         filename: '[name].bundle.js',
